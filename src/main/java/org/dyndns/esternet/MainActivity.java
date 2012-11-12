95a52a90c1705072a6a06bc8b301eb299f4c2e60
esterren <renato.estermann@gmail.com>
2012-11-12 21:07:16 +0100

    added listview

--- src/main/java/org/dyndns/esternet/MainActivity.java
+++ src/main/java/org/dyndns/esternet/MainActivity.java
@@ -1,25 +1,40 @@
 package org.dyndns.esternet;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
-    private static String TAG = "bodyperformer";
+	private static String TAG = "bodyperformer";
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	/**
+	 * Called when the activity is first created.
+	 * 
+	 * @param savedInstanceState
+	 *            If the activity is being re-initialized after previously being
+	 *            shut down then this Bundle contains the data it most recently
+	 *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
+	 *            is null.</b>
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		Log.i(TAG, "onCreate");
-        setContentView(R.layout.main);
-    }
+		// setContentView(R.layout.main);
 
+		String[] values = new String[] { "Track Route", "Create Activity", "Change weight" };
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.main, R.id.label, values);
+		setListAdapter(adapter);
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		String item = (String) getListAdapter().getItem(position);
+		Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+	}
 }
-
